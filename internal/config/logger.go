6e3fba370b775624e462ed0bfe2996804d68dd04
saulo-duarte <saulolduarte@outlook.com>
2025-09-07 15:27:08 -0300

    feat: Refactor logger initialization and add context support for log entries

--- internal/config/logger.go
+++ internal/config/logger.go
@@ -1,13 +1,29 @@
 package config
 
 import (
+	"context"
+	"os"
+
 	"github.com/sirupsen/logrus"
 )
 
-var Log *logrus.Logger
+var Logger *logrus.Logger
 
 func Init() {
-	Log = logrus.New()
-	Log.SetFormatter(&logrus.JSONFormatter{})
-	Log.SetLevel(logrus.InfoLevel)
+	Logger = logrus.New()
+
+	Logger.SetFormatter(&logrus.JSONFormatter{})
+
+	if level, err := logrus.ParseLevel(os.Getenv("LOG_LEVEL")); err == nil {
+		Logger.SetLevel(level)
+	} else {
+		Logger.SetLevel(logrus.InfoLevel)
+	}
+}
+
+func WithContext(ctx context.Context) *logrus.Entry {
+	requestID, _ := ctx.Value("requestID").(string)
+	return Logger.WithFields(logrus.Fields{
+		"lambda_request_id": requestID,
+	})
 }
