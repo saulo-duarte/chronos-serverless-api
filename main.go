89a2fe58cabe91aed82fb16703f63ca9f1f201ff
saulo-duarte <saulolduarte@outlook.com>
2025-09-13 19:44:06 -0300

    feat: Refactor main.go to integrate container for dependency injection and remove unused code

--- main.go
+++ main.go
@@ -3,37 +3,25 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
 	"github.com/go-chi/chi/v5"
-	"github.com/saulo-duarte/chronos-lambda/internal/auth"
-	"github.com/saulo-duarte/chronos-lambda/internal/config"
+
+	"github.com/saulo-duarte/chronos-lambda/internal/container"
 	"github.com/saulo-duarte/chronos-lambda/internal/router"
-	"github.com/saulo-duarte/chronos-lambda/internal/user"
 )
 
 var chiLambda *chiadapter.ChiLambdaV2
 
 func init() {
-	config.Init()
-	auth.Init()
-	config.InitCrypto()
-	auth.InitOauth()
-
-	dsn := os.Getenv("DATABASE_DSN")
-	if err := config.Connect(context.Background(), dsn); err != nil {
-		log.Fatalf("failed to connect to DB: %v", err)
-	}
-
-	userRepo := user.NewRepository(config.DB)
-	userService := user.NewService(userRepo)
-	userHandler := user.NewHandler(userService)
+	c := container.New()
 
 	r := router.New(router.RouterConfig{
-		UserHandler: userHandler,
+		UserHandler:    c.UserContainer.Handler,
+		ProjectHandler: c.ProjectContainer.Handler,
+		TaskHandler:    c.TaskContainer.Handler,
 	})
 
 	chiLambda = chiadapter.NewV2(r.(*chi.Mux))
@@ -45,10 +33,7 @@ func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.AP
 	resp, err := chiLambda.ProxyWithContextV2(ctx, req)
 	if err != nil {
 		log.Printf("ERROR: ProxyWithContextV2 returned an error: %v\n", err)
-	} else {
-		log.Printf("DEBUG: Response generated: %+v\n", resp)
 	}
-
 	return resp, err
 }
 
