dc59f861962165c11eea55cc7374b07432e42adb
Saulo Duarte <saulolduarte@outlook.com>
2025-09-17 16:54:01 -0300

    feat: Add StudyTopicId field to Task entity for improved task management

--- internal/task/entitity.go
+++ internal/task/entitity.go
@@ -5,24 +5,27 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/saulo-duarte/chronos-lambda/internal/project"
+	studytopic "github.com/saulo-duarte/chronos-lambda/internal/study_topic"
 	"github.com/saulo-duarte/chronos-lambda/internal/user"
 )
 
 type Task struct {
-	ID                    uuid.UUID       `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
-	GoogleCalendarEventId string          `json:"google_calendar_event_id"`
-	Name                  string          `json:"name"`
-	Description           string          `json:"description"`
-	Status                TaskStatus      `json:"status"`
-	Type                  TaskType        `json:"type"`
-	Priority              TaskPriority    `json:"priority"`
-	StartDate             *time.Time      `json:"start_date"`
-	DueDate               *time.Time      `json:"due_date"`
-	ProjectId             uuid.UUID       `json:"project_id"`
-	Project               project.Project `gorm:"foreignKey:ProjectId" json:"project"`
-	UserID                uuid.UUID       `gorm:"column:user_id;not null" json:"user_id"`
-	User                  user.User       `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"-"`
-	DoneAt                *time.Time      `json:"done_at"`
-	CreatedAt             time.Time       `json:"created_at"`
-	UpdatedAt             time.Time       `json:"updated_at"`
+	ID                    uuid.UUID             `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
+	GoogleCalendarEventId string                `json:"google_calendar_event_id"`
+	Name                  string                `json:"name"`
+	Description           string                `json:"description"`
+	Status                TaskStatus            `json:"status"`
+	Type                  TaskType              `json:"type"`
+	Priority              TaskPriority          `json:"priority"`
+	StartDate             *time.Time            `json:"start_date"`
+	DueDate               *time.Time            `json:"due_date"`
+	ProjectId             *uuid.UUID            `json:"project_id"`
+	Project               project.Project       `gorm:"foreignKey:ProjectId" json:"project"`
+	StudyTopicId          *uuid.UUID            `json:"study_topic_id"`
+	StudyTopic            studytopic.StudyTopic `gorm:"foreignKey:StudyTopicId" json:"study_topic"`
+	UserID                uuid.UUID             `gorm:"column:user_id;not null" json:"user_id"`
+	User                  user.User             `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"-"`
+	DoneAt                *time.Time            `json:"done_at"`
+	CreatedAt             time.Time             `json:"created_at"`
+	UpdatedAt             time.Time             `json:"updated_at"`
 }
