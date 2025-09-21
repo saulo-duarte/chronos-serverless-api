ba48bffd60a37ff80fcc8d04754e9e344d4ac7b6
saulo-duarte <saulolduarte@outlook.com>
2025-09-21 16:02:52 -0300

    feat: Enhance NewTaskContainer to accept projectService, studyTopicRepo, and eventHandler parameters

--- internal/task/container.go
+++ internal/task/container.go
@@ -1,14 +1,23 @@
 package task
 
-import "gorm.io/gorm"
+import (
+	"github.com/saulo-duarte/chronos-lambda/internal/project"
+	studytopic "github.com/saulo-duarte/chronos-lambda/internal/study_topic"
+	"gorm.io/gorm"
+)
 
 type TaskContainer struct {
 	Handler *Handler
 }
 
-func NewTaskContainer(db *gorm.DB) *TaskContainer {
+func NewTaskContainer(
+	db *gorm.DB,
+	projectService project.ProjectService,
+	studyTopicRepo studytopic.StudyTopicRepository,
+	eventHandler EventHandler,
+) *TaskContainer {
 	repo := NewRepository(db)
-	service := NewService(repo)
+	service := NewService(repo, projectService, studyTopicRepo, eventHandler)
 	handler := NewHandler(service)
 
 	return &TaskContainer{
